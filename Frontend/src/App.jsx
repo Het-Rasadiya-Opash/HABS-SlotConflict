fc02c69056eeb67112ccd9d2d4e2158d554d3594
Het <hra.opash@gmail.com>
2026-04-28 11:30:05 +0530

    Routing & redux setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router";
+import LoginPage from "./pages/LoginPage";
+import Register from "./pages/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div className='text-red-900'>
-      App
-    </div>
-  )
-}
+    <div className="flex flex-col min-h-screen bg-gray-50 font-sans text-gray-900">
+      <main className="flex-grow">
+        <Routes>
+           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<Register />} />
 
-export default App
+          <Route element={<ProtectedRoute />}></Route>
+        </Routes>
+      </main>
+    </div>
+  );
+};
+
+export default App;
